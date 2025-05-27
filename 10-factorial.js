be568d1a08bd633b43350af0dc1ab2b6949418e3
Brian Letting <brianletting46@gmail.com>
2025-05-27 13:25:04 +0300

    10-factorial.js

---
+++ 10-factorial.js
@@ -0,0 +1,16 @@
+function factorial(n) {
+  if (isNaN(n)) {
+    return 1;
+  }
+
+  n = parseInt(n);
+
+  if (n <= 1) {
+    return 1;
+  }
+
+  return n * factorial(n - 1);
+}
+
+const arg = process.argv[2];
+console.log(factorial(arg));
